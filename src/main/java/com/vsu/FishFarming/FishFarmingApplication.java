9559e570c0072ad89c6e14a755217b17ad655310
Mikhail Goncharov <assc-1@yandex.ru>
2022-03-26 01:24:09 +0300

    Initial commit

---
+++ src/main/java/com/vsu/FishFarming/FishFarmingApplication.java
@@ -0,0 +1,13 @@
+package com.vsu.FishFarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishFarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishFarmingApplication.class, args);
+	}
+
+}
